ec3ddc5ee853fad4adefabcdcdd2eed6744eed2b
jbred <47703524+jbredwards@users.noreply.github.com>
2026-09-13 13:27:06 -0400

    Improve compat with CraftTweaker

--- src/main/java/git/jbredwards/smithing_table/mod/common/compat/crafttweaker/CRTSmithingTemplates.java
+++ src/main/java/git/jbredwards/smithing_table/mod/common/compat/crafttweaker/CRTSmithingTemplates.java
@@ -18,20 +18,26 @@ package git.jbredwards.smithing_table.mod.common.compat.crafttweaker;
 
 import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.creativetabs.ICreativeTab;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import git.jbredwards.smithing_table.api.SmithingSlotInfo;
 import git.jbredwards.smithing_table.api.SmithingTemplate;
 import git.jbredwards.smithing_table.mod.SmithingTable;
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistry;
 import org.jetbrains.annotations.ApiStatus;
-import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -58,33 +64,146 @@ public final class CRTSmithingTemplates
     }
 
     @ZenMethod
-    public static void register(@Nullable final String name, @Optional(valueLong = 64) final int stackSize, @Optional final boolean enchantedEffect) {
-        if(name == null) {
-            CraftTweakerAPI.logError("The registry name of the smithing template must be non-null!");
-            return;
-        } else if(INITIALISED) {
-            CraftTweakerAPI.logError("Smithing templates must registered in preInit. Tried to register " + name + " too late!");
-            return;
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @ZenClass
+    public static class Builder
+    {
+        @Nonnull private final SmithingTemplate propertyHolder = new SmithingTemplate(new ModelResourceLocation("missingno"), false);
+        @Nullable private ResourceLocation guiSlotTex = SmithingSlotInfo.EMPTY_SLOT_TEMPLATE;
+        @Nullable private ModelResourceLocation model;
+        @Nullable private ResourceLocation name;
+
+        @Nonnull
+        @ZenMethod
+        public Builder creativeTabs(@Nullable final ICreativeTab[] tabs) {
+            propertyHolder.creativeTabs = tabs == null || tabs.length == 0 ? null :
+                    Arrays.stream(tabs).map(CraftTweakerMC::getCreativeTabs).toArray(CreativeTabs[]::new);
+            return this;
+        }
+        
+        /*
+        // CrT doesn't allow for custom ingredients, and its nbt ingredient doesn't allow for "anyDamage()".
+        // To work around this, apply the smithing template nbt to a generic custom item, and give that durability.
+        // The smithing table gui will read any smithing template instance from the nbt!
+        @Nonnull
+        @ZenMethod
+        public Builder durability(final int durability) {
+            propertyHolder.maxDurability = durability;
+            return this;
+        }
+        */
+
+        @Nonnull
+        @ZenMethod
+        public Builder enchantedEffect() {
+            propertyHolder.forceEnchantGlint = true;
+            return this;
         }
 
-        @Nonnull final ResourceLocation loc;
-        @Nonnull final ModelResourceLocation model;
-        if(name.indexOf('#') == -1) {
-            loc = new ResourceLocation("crafttweaker", name);
-            model = new ModelResourceLocation(loc, "inventory");
-        } else {
-            model = new ModelResourceLocation("crafttweaker:" + name);
-            loc = new ResourceLocation(model.getNamespace(), model.getPath());
+        @Nonnull
+        @ZenMethod
+        public Builder stackSize(final int stackSize) {
+            propertyHolder.maxStackSize = stackSize;
+            return this;
         }
 
-        if(TEMPLATES.containsKey(loc.getPath())) {
-            CraftTweakerAPI.logError("The registry name of the smithing template must not match an already registered smithing template!");
-            return;
+        /**
+         * Looks like CrT can only use EnumRarity?
+         * <a href="https://docs.blamejared.com/1.12/en/Mods/ContentTweaker/Vanilla/Creatable_Content/Item/#zenproperties">Source</a>
+         */
+        @Nonnull
+        @ZenMethod
+        public Builder rarity(@Nonnull final EnumRarity rarity) {
+            propertyHolder.rarity = rarity;
+            return this;
         }
 
-        @Nonnull final SmithingTemplate template = new SmithingTemplate(model, false);
-        template.forceEnchantGlint = enchantedEffect;
-        template.maxStackSize = MathHelper.clamp(stackSize, 1, 64);
-        TEMPLATES.put(loc.getPath(), template.setRegistryName(loc));
+        @Nonnull
+        @ZenMethod
+        public Builder templateSlotTexture(@Nonnull final String texture) {
+            guiSlotTex = new ResourceLocation(texture);
+            return this;
+        }
+        
+        @Nonnull
+        @ZenMethod
+        public Builder equipmentSlotInfo(@Nonnull final String hoverText, @Nonnull final String[] textures) {
+            propertyHolder.equipmentSlotInfo = new SmithingSlotInfo.Impl(new TextComponentTranslation(hoverText),
+                    Arrays.stream(textures).map(ResourceLocation::new).toArray(ResourceLocation[]::new));
+            return this;
+        }
+
+        @Nonnull
+        @ZenMethod
+        public Builder materialSlotInfo(@Nonnull final String hoverText, @Nonnull final String[] textures) {
+            propertyHolder.materialSlotInfo = new SmithingSlotInfo.Impl(new TextComponentTranslation(hoverText),
+                    Arrays.stream(textures).map(ResourceLocation::new).toArray(ResourceLocation[]::new));
+            return this;
+        }
+
+        @Nonnull
+        @ZenMethod
+        public Builder model(@Nonnull final String location) {
+            model = location.indexOf('#') != -1 ? new ModelResourceLocation(location) : new ModelResourceLocation(location, "inventory");
+            return this;
+        }
+
+        @Nonnull
+        @ZenMethod
+        public Builder name(@Nonnull final String path) {
+            name = new ResourceLocation("crafttweaker", path);
+            return this;
+        }
+
+        @ZenMethod
+        public boolean validate() {
+            if(propertyHolder.maxDurability < 0 || propertyHolder.maxDurability >= OreDictionary.WILDCARD_VALUE) return error(
+                    "Must have 0 - " + OreDictionary.WILDCARD_VALUE + " durability, but found " + propertyHolder.maxDurability);
+            if(propertyHolder.maxStackSize < 0 || propertyHolder.maxStackSize > 64) return error(
+                    "Must have 0 - " + OreDictionary.WILDCARD_VALUE + " stack size, but found " + propertyHolder.maxStackSize);
+            if(propertyHolder.rarity == null) return error("Rarity cannot be null");
+            if(guiSlotTex == null) return error("Template slot texture cannot be null");
+
+            final boolean missingModel = model == null;
+            final boolean missingName = name == null;
+            
+            if(missingModel && missingName) return error("Must have a model or name");
+            else if(missingModel) model = new ModelResourceLocation(name, "inventory");
+            else if(missingName) name = new ResourceLocation("crafttweaker", model.getPath());
+
+            if(INITIALISED) return error("Smithing templates must registered in preInit. Tried to register " + name + " too late!");
+            else if(TEMPLATES.containsKey(name.getPath())) return error("The registry name of " + name + " must not match an already registered smithing template!");
+            return true;
+        }
+
+        protected static boolean error(@Nonnull final String msg) {
+            CraftTweakerAPI.logError(msg);
+            return false;
+        }
+
+        @ZenMethod
+        public void register() {
+            if(!validate()) return;
+
+            @Nonnull final SmithingTemplate template = build().setRegistryName(name);
+            template.creativeTabs = propertyHolder.creativeTabs;
+            template.forceEnchantGlint = propertyHolder.forceEnchantGlint;
+            template.maxDurability = propertyHolder.maxDurability;
+            template.maxStackSize = propertyHolder.maxStackSize;
+            template.rarity = propertyHolder.rarity;
+            template.equipmentSlotInfo = propertyHolder.equipmentSlotInfo;
+            template.materialSlotInfo = propertyHolder.materialSlotInfo;
+
+            TEMPLATES.put(name.getPath(), template);
+        }
+
+        @Nonnull
+        @ApiStatus.OverrideOnly
+        protected SmithingTemplate build() {
+            return new SmithingTemplate(model, guiSlotTex, false);
+        }
     }
 }
